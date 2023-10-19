adc121693e25166f7191c0d281c5162d88ba3af8
Kalpesh <AWSStudyKalpesh@gmail.com>
2023-10-19 17:42:18 +0530

    comming both files

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
+provider "aws"{
+    region = "us-east-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-041feb57c611358bd"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "Elasticsearch"
 }
--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -1,36 +1,34 @@
-provider "aws" {
-  region = "us-east-1"
+provider "aws"{
+    region = "us-east-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-041feb57c611358bd"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "Elasticsearch"
     security_groups = [ "demo-sg" ]
 }
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
-  
+
   ingress {
-    description      = "Shh access"
+    description      = "SSH Access"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    }
+  }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "ssh-prot"
-
+    Name = "ssh-port"
   }
 }
